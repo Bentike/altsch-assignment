b8cc9490f12c7f5f3ff374bbf267e6291e4bbfd5
Bentike <ajadiyusuf73@gmail.com>
2022-10-22 15:58:58 +0100

    added Component folder

---
+++ src/Component/UserCard.js
@@ -0,0 +1,9 @@
+const UserCard = () => {
+    return(
+        <div>
+            <h1>User Card</h1>
+        </div>
+    )
+}
+
+export default UserCard;
---
+++ src/Component/Users.js
@@ -0,0 +1,11 @@
+import UserCard from "./UserCard";
+const Users = () => {
+    return(
+        <div>
+            <h1>Users Page</h1>
+            <UserCard/>
+        </div>
+    )
+}
+
+export default Users;
