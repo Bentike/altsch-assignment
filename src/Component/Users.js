fb022ca4f0a7f6ced7e4a49b37b2e45a4f017f0e
Bentike <ajadiyusuf73@gmail.com>
2022-10-23 23:02:19 +0100

    added data

--- src/Component/Users.js
+++ src/Component/Users.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import UserCard from "./UserCard";
+
 const Users = () => {
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch("https://randomuser.me/api/?results=5000")
+          .then(response => response.json())
+          .then(data => console.log(data.results));
+          setIsLoading(false);
+        }, []);
+
     return(
         <div>
             <h1>Users Page</h1>
-            <UserCard/>
+            {isLoading ? "Loading..." : <UserCard/>}
+            {/* {<UserCard/>} */}
         </div>
     )
 }
