f22775c2baef7d3ac928a3abbddea82d13dbd526
Bentike <ajadiyusuf73@gmail.com>
2022-10-25 13:13:22 +0100

    added user photos to UserCard

--- src/Component/UserCard.js
+++ src/Component/UserCard.js
@@ -1,11 +1,11 @@
 import './Styles/UserCard.css';
 
 const UserCard = (props) => {
-    console.log(props.data.picture.large);
+    const large = props.data.picture.large;
     const {gender, email, phone} = props.data;    
     return(
         <div className="card">
-            <img alt="user"/>
+            <img src={large} alt="user"/>
             <p className="cardcontent">{gender}</p>
             <p className="cardcontent">{email}</p>
             <p className="cardcontent">{phone}</p>
