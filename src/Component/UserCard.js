53d27f476e502ee384cfba07018001e7bd9a8790
Bentike <ajadiyusuf73@gmail.com>
2022-10-25 14:21:07 +0100

    displayed user's  fullname UserCard

--- src/Component/UserCard.js
+++ src/Component/UserCard.js
@@ -2,10 +2,12 @@ import './Styles/UserCard.css';
 
 const UserCard = (props) => {
     const large = props.data.picture.large;
-    const {gender, email, phone} = props.data;    
+    const {gender, email, phone} = props.data;
+    const {title, first, last} = props.data.name;    
     return(
         <div className="card">
             <img src={large} alt="user"/>
+            <p>{title} {first} {last}</p>
             <p className="cardcontent">{gender}</p>
             <p className="cardcontent">{email}</p>
             <p className="cardcontent">{phone}</p>
